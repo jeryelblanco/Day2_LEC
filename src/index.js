187b12833d6fd05f2ac3e3bdb53e42f1cbec1259
Jeryel Blanco <jeryelblanco@gmail.com>
2023-02-22 15:11:39 -0500

    Solution

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // console.log(bookStore);
 
 function formatPrice(price) {
-  return '$' + Number.parseFloat(price).toFixed(2);
+  return '$' + parseFloat(price).toFixed(2);
 }
 
 // create a function called renderBook(book)
@@ -19,5 +19,36 @@ function formatPrice(price) {
   //   <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
   //   <button>Delete</button>
   // </li>
+console.log(bookStore)
+  function renderBook(book){
+  const bookList = document.getElementById("book-list")
+  const li = document.createElement("li")
+  li.className = "list-li"
+  bookList.append(li)
+  const bookTitle = document.createElement("h3")
+  const bookAuthor = document.createElement("p")
+  const bookPrice = document.createElement("p")
+  const bookImg = document.createElement("img")
+  const bookBtn = document.createElement("button")
+  li.append(bookTitle, bookAuthor, bookPrice, bookImg, bookBtn)
+  bookTitle.innerHTML = book.title
+  bookAuthor.innerText = book.author
+  bookPrice.textContent = formatPrice(book.price)
+  bookImg.src = book.imageUrl
+  bookBtn.innerHTML = "Delete"
+  bookBtn.addEventListener("click", ()=>{
+  li.remove()
+  })
+}
+
+bookStore.inventory.forEach((data, index) => {
+if(index > 0){
+renderBook(data)
+}
+})
+
+
+
+
 
 
